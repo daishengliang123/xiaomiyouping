e26ef6d8e75283515a090db5fd6dd94ecd05405e
代胜亮 <1596747222@qq.com>
2021-06-24 17:28:54 +0800

    xx

--- youpin/src/router/index.js
+++ youpin/src/router/index.js
@@ -1,29 +1,57 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import Index from '@/views/index.vue';
+import Login from '@/views/login.vue';
+
+// import home from '@/views/home/home.vue';
+// import classification from '@/views/classification/classification.vue';
+// import shopcart from '@/views/shopcart/shopcart.vue';
+// import mine from '@/views/mine/mine.vue';
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'index',
+        component: Index,
+        redirect: '/home',
+        children:[
+            {
+                path:'home',
+                name:'home',
+                component: () => import('@/views/home/home.vue')
+            },
+            {
+                path:'classification',
+                name:'classification',
+                component: () => import('@/views/classification/classification.vue')
+            },
+            {
+                path:'shopcart',
+                name:'shopcart',
+                component: () => import('@/views/shopcart/shopcart.vue')
+            },
+            {
+                path:'mine',
+                name:'mine',
+                component: () => import('@/views/mine/mine.vue')
+            },
+        ]
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ youpin/src/views/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <transition>
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
+    <van-tabbar
+      v-model="active"
+      active-color="#845f3f"
+      inactive-color="#666666"
+    >
+      <van-tabbar-item icon="home-o">
+        <router-link to="/home" tag="div" append>首页</router-link>
+      </van-tabbar-item>
+      <van-tabbar-item icon="coupon-o">
+        <router-link to="/classification" tag="div" append>分类</router-link>
+      </van-tabbar-item>
+      <van-tabbar-item icon="cart-o">
+        <router-link to="/shopcart" tag="div" append>购物车</router-link>
+      </van-tabbar-item>
+      <van-tabbar-item icon="user-o">
+        <router-link to="/mine" tag="div" append>我的</router-link>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.router-link-active {
+  font-weight: 600;
+}
+</style>
